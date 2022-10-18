d900a8f4586c873304214cb203a3a89329b881f1
Naveed <naveed5651@gmail.com>
2022-10-18 19:50:11 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [initValue, setNumber] = useState(0);
+
+  const addHandler = () => {
+    setNumber(initValue + 1);
+  };
+  const subtractHandler = () => {
+    setNumber(initValue - 1);
+  };
+
+  const resetHandler = () => {
+    setNumber(0);
+  };
+  let borderColor = "#f7fafd";
+  if (initValue === 0) {
+    borderColor = "#f7fafd";
+  } else if (initValue > 0) {
+    borderColor = "#588157";
+  } else {
+    borderColor = "#e56b6f";
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Counter application</h1>
+        <div className="circle border" style={{ borderColor }}>
+          <p className="number num-color ">{initValue}</p>
+        </div>
+        <div className="btn">
+          <button className="add" onClick={addHandler}>
+            {" "}
+            Add
+          </button>
+          <button className="subtract" onClick={subtractHandler}>
+            {" "}
+            Subtract
+          </button>
+          <button className="reset" onClick={resetHandler}>
+            {" "}
+            reset
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
